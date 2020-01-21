cfeaca959cf4cd06656d922576fbeaf01eb3735d
Raymond Chan <raymondcja@gmail.com>
2020-01-22 00:11:56 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+// Components
+import Navbar from "./components/Navbar";
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Signup page</h1>
+            </div>
+        )
+    }
+}
+
+export default Signup
