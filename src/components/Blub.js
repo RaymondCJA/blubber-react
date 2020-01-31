fc7c334d9bf40e322a699cd613f45b759877d93f
Raymond Chan <raymondcja@gmail.com>
2020-01-31 15:05:26 +1000

    finished blub (posts) card info

--- src/components/Blub.js
+++ src/components/Blub.js
@@ -1,32 +1,65 @@
-import React, { Component } from 'react'
-import withStyles from '@material-ui/core/styles/withStyles';
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "react-router-dom/Link";
 
 // MUI STUFF
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { Typography } from '@material-ui/core';
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { Typography } from "@material-ui/core";
 
 const styles = {
-    card: {
-        display: 'flex'
-    }
-}
+  card: {
+    display: "flex",
+    marginBottom: 20
+  },
+  image: {
+    minWidth: 200
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover"
+  }
+};
 
 export class Blub extends Component {
-    render() {
-        const { classes, blub : { body, createdAt, userImage, userHandle, blubId, likeCount, commentCount } } = this.props // const classes = this.prop.classes; (destructuring)
-        return (
-            <Card>
-                <CardMedia image={userImage} title="Profile Image"/>
-                <CardContent>
-                <Typography variant="h5">{userHandle}</Typography>
-                    <Typography variant="body2" color="textSecondary">{createdAt}</Typography>
-                    <Typography variant="body1">{body}</Typography>
-                </CardContent>
-            </Card>
-        )
-    }
+  render() {
+    const {
+      classes,
+      blub: {
+        body,
+        createdAt,
+        userImage,
+        userHandle,
+        blubId,
+        likeCount,
+        commentCount
+      }
+    } = this.props; // const classes = this.prop.classes; (destructuring)
+    return (
+      <Card className={classes.card}>
+        <CardMedia
+          image={userImage}
+          title="Profile Image"
+          className={classes.image}
+        />
+        <CardContent class={classes.content}>
+          <Typography
+            variant="h5"
+            component={Link}
+            to={`/users/${userHandle}`}
+            color="primary"
+          >
+            {userHandle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {createdAt}
+          </Typography>
+          <Typography variant="body1">{body}</Typography>
+        </CardContent>
+      </Card>
+    );
+  }
 }
 
-export default withStyles(styles)(Blub)
+export default withStyles(styles)(Blub);
