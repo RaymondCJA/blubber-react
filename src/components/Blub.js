b15f54b9b53f76c6bc2e4664bd237bc1a816a44d
Raymond Chan <raymondcja@gmail.com>
2020-02-01 07:39:05 +1000

    login form helper text from material-ui has been deprecated

--- src/components/Blub.js
+++ src/components/Blub.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 // MUI STUFF
 import Card from "@material-ui/core/Card";
@@ -24,6 +26,7 @@ const styles = {
 
 export class Blub extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       blub: {
@@ -43,7 +46,7 @@ export class Blub extends Component {
           title="Profile Image"
           className={classes.image}
         />
-        <CardContent class={classes.content}>
+        <CardContent className={classes.content}>
           <Typography
             variant="h5"
             component={Link}
@@ -53,7 +56,7 @@ export class Blub extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import { Grid } from "@material-ui/core";
 import axios from "axios";
 
-import Blub from '../components/Blub';
+import Blub from "../components/Blub";
 
 export class home extends Component {
   state = {
     blubs: null
   };
   componentDidMount() {
-    axios.get("/blubs")
+    axios
+      .get("/blubs")
       .then(res => {
-        console.log(res.data);
         this.setState({
           blubs: res.data
         });
@@ -20,7 +20,7 @@ export class home extends Component {
   }
   render() {
     let recentBlubsMarkup = this.state.blubs ? (
-      this.state.blubs.map(blub => <Blub blub={blub}/>)
+      this.state.blubs.map(blub => <Blub key={blub.blubId} blub={blub} />)
     ) : (
       <p>Loading...</p>
     );
